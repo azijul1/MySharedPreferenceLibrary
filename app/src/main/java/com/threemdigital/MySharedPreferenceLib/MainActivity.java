3a80104d817c17afa3bf05c5ca2ac03d44013ad5
Dhananjay <d.gupta@3mdigital.co.in>
2017-12-31 13:21:16 +0530

    demo example added

--- app/src/main/java/com/threemdigital/MySharedPreferenceLib/MainActivity.java
+++ app/src/main/java/com/threemdigital/MySharedPreferenceLib/MainActivity.java
@@ -3,12 +3,24 @@ package com.threemdigital.MySharedPreferenceLib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.mobtexting.sharedprefrencelib.MySharedPrefrennce;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView txtv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtv=(TextView)findViewById(R.id.txtv);
+
+        MySharedPrefrennce mySharedPrefrennce=MySharedPrefrennce.getInstance(getBaseContext());
+        mySharedPrefrennce.saveStringData("test","dhananjay");
+
+
+        String dataStr=mySharedPrefrennce.getStringData("test");
+        txtv.setText(dataStr);
     }
 }
